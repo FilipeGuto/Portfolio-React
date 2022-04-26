7ddffc8cec4132edcb86ecefd4b9c68503cc5018
FilipeGuto <filipeguto95@gmail.com>
2022-04-26 11:03:28 -0300

    FEAT: Adição de novo projeto e mudança na lógica de botão com navegação lateral

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -6,6 +6,7 @@ import Project3 from '../../img/port-3.png';
 import Project4 from '../../img/port-4.png';
 import Project5 from '../../img/port-5.png';
 import Project6 from '../../img/port-6.png';
+import Project7 from '../../img/port-7.png';
 
 function Project() {
     return (
@@ -112,6 +113,21 @@ function Project() {
                                 <h5 className="project__text">App de Receitas</h5>
                                 <h4 className="project__text">Projeto feito em grupo, em React feito para uso em mobile</h4>
                                 <a href="https://project-app-receitas.vercel.app/"  target="_blank" className="project__btn" rel="noreferrer">Ver Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col__3">
+                        <div className="project__box pointer relative">
+                            <div className="project__box__img pointer relative">
+                                <div className="project__img__box">
+                                    <img src={Project7} alt="" className="project__img" />
+                                </div>
+                                <div className="mask__effect"></div>
+                            </div>
+                            <div className="project__meta absolute">
+                                <h5 className="project__text">Wishlist</h5>
+                                <h4 className="project__text">Projeto com base em uma lista de presente,feito em React com uso de bootstrap para todo layout</h4>
+                                <a href="https://wish-list-weld.vercel.app/"  target="_blank" className="project__btn" rel="noreferrer">Ver Detalhes</a>
                             </div>
                         </div>
                     </div>
