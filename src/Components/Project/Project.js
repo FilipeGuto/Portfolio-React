2b19f6077116a34f92baaa8c74a7e54c4b70e559
FilipeGuto <filipeguto95@gmail.com>
2022-05-16 16:44:22 -0300

    FEAT: Adição de novo projeto

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -7,6 +7,7 @@ import Project4 from '../../img/port-4.png';
 import Project5 from '../../img/port-5.png';
 import Project6 from '../../img/port-6.png';
 import Project7 from '../../img/port-7.png';
+import Project8 from '../../img/port-8.png';
 
 function Project() {
     return (
@@ -128,6 +129,21 @@ function Project() {
                                 <h5 className="project__text">Wishlist</h5>
                                 <h4 className="project__text">Projeto com base em uma lista de presente,feito em React com uso de bootstrap para todo layout</h4>
                                 <a href="https://wish-list-weld.vercel.app/"  target="_blank" className="project__btn" rel="noreferrer">Ver Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col__3">
+                        <div className="project__box pointer relative">
+                            <div className="project__box__img pointer relative">
+                                <div className="project__img__box">
+                                    <img src={Project8} alt="" className="project__img" />
+                                </div>
+                                <div className="mask__effect"></div>
+                            </div>
+                            <div className="project__meta absolute">
+                                <h5 className="project__text">Coin Store</h5>
+                                <h4 className="project__text">Projeto Full Stack, de um e-commerce, com testes para back e front e deploy</h4>
+                                <a href="https://deft-moxie-065dae.netlify.app/"  target="_blank" className="project__btn" rel="noreferrer">Ver Detalhes</a>
                             </div>
                         </div>
                     </div>
